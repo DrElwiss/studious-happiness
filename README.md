a270c52999079372aeb7388ea7ca7aaaaf39fa33
DrElwiss <123003292+DrElwiss@users.noreply.github.com>
2023-01-20 20:43:47 +0300

    12

---
+++ README.md
@@ -0,0 +1,41 @@
+# SCUM game discord killfeed grab
+
+
+![alt get this in config.json: chat_id to source_channel, auth-token to authorization and cookie](https://github.com/artskar/scum-killfeed/blob/main/src/data4bot.png)
+1) Take a look on a picture and get the same way data to [auth.json](https://github.com/artskar/scum-killfeed/blob/main/src/auth.json):
+```
+{
+    ...
+    source_channel: chat_id,
+    authorization: auth-token,
+    cookie: cookie,
+}
+```
+
+2) Add your squad nicknames and change other settings in [config.json](https://github.com/artskar/scum-killfeed/blob/main/src/config.json):
+```
+{
+    ...
+    names: ["name1", "name2"], // Squad nicknames
+    trapkills_only: "false", // Send in channel only trapkills from your Squad
+    timezone: '1' // Default Moscow timezone in diff with bot-hosting server time
+}
+```
+
+3) You also have to find out how to make a bot with discord API. and get a token for the "**token**" field in [auth.json](https://github.com/artskar/scum-killfeed/blob/main/src/auth.json)
+and get your own channel id to place it in "**target_channel**".
+
+    [RU | Посмотреть как создать и добавить бота на сервер и узнать id канала и токен бота](https://vc.ru/services/288966-bot-discord-kak-sozdat-i-dobavit-na-server)
+
+    [EN | How to Create a Bot in Discord (1-3 step)](https://www.wikihow.com/Create-a-Bot-in-Discord)
+
+
+```
+{
+    token: bot_token,
+    target_channel: channel_id,
+    ...
+}
+```
+
+4) You can use **bot-hosting.net** to run this bot almost for free. 
---
+++ index.js
@@ -0,0 +1,135 @@
+import { Client, Intents } from 'discord.js';
+import fetch from 'node-fetch';
+import auth from './auth.json' assert { type: "json" };
+import config from './config.json' assert { type: "json" };
+import { getEmbedKill, getKillLog, parseKillData, parseKillToStatus } from './kills.utils.js';
+
+const { authorization, source_channel, target_channel, cookie, token, debug } = auth;
+const { with_logs } = config;
+
+const isProduction = debug !== 'true';
+const withLogs = with_logs === 'true';
+
+const messagesCount = 10;
+const requestTime = 30 * 1000;
+
+const robot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const errorState = {
+  isError: false,
+  timer: 2.5,
+  setError(state) {
+    this.isError = state;
+    if (state === true) {
+      this.updateTimer();
+    } else {
+      this.resetTimer();
+    }
+  },
+  updateTimer() {
+    this.timer = this.timer * 2;
+  },
+  resetTimer() {
+    this.timer = 2.5;
+  }
+};
+
+const isContainId = idArray => ({ id }) => idArray.findIndex(item => item.id === id) < 0;
+
+const fetchEffect = (prevKillfeed = []) => {
+  const channel = robot.channels.cache.get(target_channel);
+
+  fetch(`https://discord.com/api/v9/channels/${source_channel}/messages?limit=${messagesCount}`, {
+    method: 'GET',
+    headers: {
+      authorization,
+      cookie
+    },
+  })
+    .then(response => response.json())
+    .then(
+      killFeed => {
+        const newKills = killFeed.filter(isContainId(prevKillfeed));
+        const newKillfeed = [...newKills, ...prevKillfeed].slice(0, messagesCount);
+        const parsedKills = newKills.reverse().map(parseKillData);
+        const kills = parsedKills.reduce((filtered, kill) => {
+          const embed = getEmbedKill(kill);
+          embed && filtered.push(embed);
+          return filtered;
+        }, []);
+
+        if (withLogs) {
+          parsedKills.map(kill => console.log(getKillLog(kill)));
+        }
+
+        if (isProduction && newKills.length) {
+          kills.map(kill => {
+            channel.send(kill);
+          });
+
+          robot.user.setActivity(parseKillToStatus(newKills.at(-1)), {
+            type: 'WATCHING',
+          });
+
+          if (errorState.isError) {
+            robot.user.setStatus({
+              status: 'online',
+            });
+            withLogs && console.log('Bot Is back on duty after an Error');
+            const isNotFirstTimeError = errorState.timer !== 2.5;
+            if (isNotFirstTimeError) {
+              channel.send(robot.user.username + ' is back on duty after an Error!');
+            }
+            errorState.setError(false);
+          }
+        }
+
+        setTimeout(() => fetchEffect(newKillfeed), requestTime);
+      },
+      reject => {
+        if (isProduction) {
+          const isFirstTimeError = errorState.timer === 2.5;
+
+          if (!isFirstTimeError) {
+            const isThirdTimeError = errorState.timer > 5;
+            const showEveryone = isThirdTimeError ? '' : '@everyone ';
+            const advice = isThirdTimeError ? '' : 'probably auth.json data update needed ';
+            channel.send(showEveryone + 'Fetch rejected, next Fetch in ' + errorState.timer + ' minutes' + '```' + JSON.stringify(reject) + '```' + advice);
+          }
+
+          robot.user.setStatus({
+            status: 'offline',
+          });
+        }
+
+        console.log('Fetch rejected, probably auth.json data update needed, next Fetch in ' + errorState.timer + ' minutes', reject);
+
+        setTimeout(() => {
+          errorState.setError(true);
+          fetchEffect(prevKillfeed);
+        }, errorState.timer * 60 * 1000);
+      });
+};
+
+robot.on('ready', function () {
+  console.log(robot.user.username + ' is on duty!');
+
+  // robot.channels.cache.get(target_channel).send(robot.user.username + ' is on duty!');
+
+  robot.user.setStatus({
+    status: 'online',
+  });
+
+  robot.user.setActivity('killfeed', {
+    type: 'WATCHING',
+  });
+
+  fetchEffect();
+});
+
+if (isProduction) {
+  robot.login(token);
+} else {
+  console.log('Debug is run!');
+  fetchEffect();
+}
